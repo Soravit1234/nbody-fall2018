ef8d59563f843e928d4c249aa9a0e61bbed51ba9
Soravit1234 <43193902+Soravit1234@users.noreply.github.com>
2018-09-17 21:25:28 -0400

    Add files via upload

--- src/Body.java
+++ src/Body.java
@@ -1,91 +1,112 @@
 
 public class Body {
-	
-	double myXPos;
-	double myYPos;
-	double myXVel;
-	double myYVel;
-	double myMass;
-	String myFileName;
+	private double myXPos;
+	private double myYPos;
+	private double myXVel;
+	private double myYVel;
+	private double myMass;
+	private String myFileName;
 
-
-
-public Body(double xp, double yp, double xv, double yv, double mass, String filename){
-		myXPos = xp;
-		myYPos = yp;	
+	public Body(double xp, double yp, double xv, double yv, double mass, String filename) { //lists parameters
+		myXPos = xp;  // Defining within the constructor
+		myYPos = yp;
 		myXVel = xv;
 		myYVel = yv;
 		myMass = mass;
 		myFileName = filename;
-}
-
-public Body(Body p){
-		myXPos = p.myXPos;
-		myYPos = p.myYPos;
-		myXVel = p.myXVel;
-		myYVel = p.myYVel;
-		myMass = p.myMass;
-		myFileName = p.myFileName;
-}
-
-public double calcDistance(Body b){
-	double bodyx = myXPos - b.myXPos;
-    double bodyy = myYPos - b.myYPos;
-    return Math.sqrt(bodyx*bodyx + bodyy*bodyy);
-}
-
-public double calcForceExertedBy(Body body){
-	double r = calcDistance(body);
-	double G = (6.67* Math.pow(10, -11));
-	double force = (G * body.myMass * myMass )/ (r*r);
-	return force;
-}
-
-public double calcForceExertedByX(Body p){  
-	double forceX = calcForceExertedBy(p) * (p.myXPos - myXPos) / calcDistance(p);
-	if (calcDistance(p) == 0){
-		return 0;
-	 }
-	 return forceX;
-}
-
-public double calcForceExertedByY(Body p){  
-	double forceY = calcForceExertedBy(p) * (p.myYPos - myYPos) / calcDistance(p);
-	if (calcDistance(p) == 0){
-		return 0;
-	 }
-	return forceY;
-}
-public double calcNetForceExertedByX(Body[] bodies){ 
-	double sum = 0;
-	for (int x=0; x < bodies.length; ++x){
-		if (! bodies.equals(this)) {
-		    sum += calcForceExertedByX(bodies[x]);
-		}
 	}
-	return sum;
-}
-public double calcNetForceExertedByY(Body[] bodies){ 
-	double sum = 0;
-	for (int y=0; y < bodies.length; ++y){
-		if (! bodies.equals(this)) {
-		    sum += calcForceExertedByY(bodies[y]);
-		}
 
+	public Body(Body b) {  // Copying into new constructor (so you know how to make objects out of other objects)
+		myXPos = b.myXPos;
+		myYPos = b.myYPos;
+		myXVel = b.myXVel;
+		myYVel = b.myYVel;
+		myMass = b.myMass;
+		myFileName= b.myFileName;
 	}
-	return sum;	
-}
 
-public void update(double seconds, double xforce, double yforce){  
-	double accelX = xforce/myMass;
-	double accelY = yforce/myMass;
-	myXVel += seconds * xforce / myMass;
-	myYVel += seconds * yforce / myMass;
-	myXPos += seconds * myXVel;
-	myYPos += seconds * myYVel;
+	public double getX() { //Methods have to be public and get values 
+		return myXPos;
+	}
+
+	public double getY() {
+		return myYPos;
+	}
+
+	public double getXVel() {
+		return myXVel;
+	}
+
+	public double getYVel() {
+		return myYVel;
+	}
+
+	public double getMass() {
+		return myMass;
+	}
+
+	public String getName() {
+		return myFileName;
+	}
+	public double calcDistance(Body b) {
+		double dx = myXPos - b.myXPos; // taking the differences between x positions
+		double dy = myYPos - b.myYPos; // taking the difference between y positions 
+		return Math.sqrt((dx *dx + dy*dy));
+  }
+	 public double calcForceExertedBy(Body p) { // (Body p) copies into a new constructor
+		double G = 6.67e-11;
+		double m1 = myMass;
+		double m2 = p.myMass; // copies in a new mass
+		double distance = calcDistance(p); // (p) uses new constructor -- thats why you don't use b in this case 
+	    return (G * m1 * m2) / (distance * distance);
+	 }
+	 public double calcForceExertedByX(Body p) {
+		 double F = calcForceExertedBy(p);
+		 double dx = -1*(myXPos - p.myXPos); 
+		 double distance = calcDistance(p);
+		 return (F * dx) / distance;
+	 }
+	 public double calcForceExertedByY(Body p) {
+		 double F = calcForceExertedBy(p);
+		 double dy = -1*(myYPos - p.myYPos);
+		 double distance = calcDistance(p);
+		 return (F * dy) / distance;
+		 }
+	 public double calcNetForceExertedByX(Body[] bodies) {
+		 double sum = 0;
+		 for(Body b : bodies) { //iterates through bodies b equals element
+			 if(! b.equals(this)) { // Body cannot equal itself -- checking if b equals this
+				 sum += this.calcForceExertedByX(b) ; // sums forces between home body and foreign body
+				 }
+			 }
+		 return sum;
+	 }
+	 public double calcNetForceExertedByY(Body[] bodies) {
+		 double sum = 0;
+		 for(Body b : bodies) { // iterates through the bodies - b is individual element
+			 if(! b.equals(this)) { // this equals home body and b equals foreign body, need both for force between
+				 sum += this.calcForceExertedByY(b); // sums forces between home and foreign body
+			 }
+		 }
+	 return sum;
+	 }
+	 public void update(double deltaT, double xforces, double yforces) {
+		 double Fx = xforces;
+		 double Fy = yforces;
+		 double Ax =  Fx / myMass;
+		 double Ay = Fy / myMass; 
+		 double nvx = myXVel + deltaT * Ax; // new vx
+		 double nvy = myYVel + deltaT * Ay; // new vy
+		 double nx = myXPos + deltaT * nvx; // new x value
+		 double ny = myYPos + deltaT * nvy; // new y value
+		 // name new instance variables
+		  myXPos = nx;
+		  myYPos = ny;
+		  myXVel = nvx;
+		  myYVel = nvy;
+	 }
+	 public void draw() {
+		 StdDraw.picture(myXPos, myYPos, "images/" + myFileName);
+		 
+	 }
 }
-public void draw(){  
-	StdDraw.picture(myXPos, myYPos, "images/" + myFileName);
-}
-}
-	
--- src/NBody.java
+++ src/NBody.java
@@ -1,7 +1,7 @@
 	
 
 /**
- * @author Soravit Kitsiriboon
+ * @author YOUR NAME THE STUDENT IN 201
  * 
  * Simulation program for the NBody assignment
  */
@@ -11,85 +11,111 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class NBody {
-
-	public static void main(String[] args) { 
-		double T = 2000000.0;
-		double dt = 25000.0;
-		String pfile = "data/planets.txt";
+	
+	/**
+	 * Read the specified file and return the radius
+	 * @param fname is name of file that can be open
+	 * @return the radius stored in the file
+	 * @throws FileNotFoundException if fname cannot be open
+	 */
+	public static double readRadius(String fname) throws FileNotFoundException  {
+		Scanner s = new Scanner(new File(fname));
+	
+		// TODO: read values at beginning of file to
+		// find the radius
+		int Number = s.nextInt(); // whole number (integer)
+		double Radius = s.nextDouble();  // reads the radius
+		
+		s.close();
+		
+		// TODO: return radius read
+		return Radius;	 // Returns the radius value
+	}
+	
+	/**
+	 * Read all data in file, return array of Celestial Bodies
+	 * read by creating an array of Body objects from data read.
+	 * @param fname is name of file that can be open
+	 * @return array of Body objects read
+	 * @throws FileNotFoundException if fname cannot be open
+	 */
+	public static Body[] readBodies(String fname) throws FileNotFoundException {
+		
+			Scanner s = new Scanner(new File(fname));
+			
+			// TODO: read # bodies, create array, ignore radius
+			int nb = s.nextInt(); // # bodies to be read (1-5)
+			s.next(); // skips second number
+			Body[] nbArray = new Body[nb]; // use Body[] because that is the array of body objects, then named nb from above
+			for(int k=0; k < nb; k++) {
+				nbArray[k] = new Body(s.nextDouble(), s.nextDouble(), s.nextDouble(), s.nextDouble(), s.nextDouble(), s.next());
+				// used new name of array and then inputed with s.nextDouble() for 5 numbers in line and then s.next() goes to new line
+				// TODO: read data for each body
+				// construct new body object and add to array
+				
+			}
+			
+			s.close();
+			
+			// TODO: return array of body objects read
+			return nbArray;
+	}
+	public static void main(String[] args) throws FileNotFoundException{
+		double totalTime = 1000000000.0;
+		double dt = 1000000.0;
+		
+		String fname= "./data/planets.txt";
 		if (args.length > 2) {
-			T = Double.parseDouble(args[0]);
+			totalTime = Double.parseDouble(args[0]);
 			dt = Double.parseDouble(args[1]);
-			pfile = args[2];
+			fname = args[2];
+		}	
+		
+		Body[] bodies = readBodies(fname);
+		for(int k = 0; k < bodies.length; k++) {  // reading through the file given and putting it into the main method
+		bodies[k].draw(); // This begins to draw the planets in the program
 		}
-		double radius = readRadius(pfile);
+		double radius = readRadius(fname);
+		
 		StdDraw.setScale(-radius, radius);
-		StdDraw.picture(0, 0, "images/starfield.jpg");
-
-		Body[] bodys = readBodies(pfile); 
-		for (int i = 0; i < bodys.length; i++) { //reads file and draws bodys
-			bodys[i].draw();
+		StdDraw.picture(0,0,"images/starfield.jpg");
+	
+		for(double t = 0.0; t < totalTime; t += dt) {
+			
+			// TODO: create double arrays xforces and yforces
+			// to hold forces on each body
+			double[] xforces = new double [bodies.length]; // creates new double array for xforces
+			double[] yforces = new double [bodies.length];
+			
+			// TODO: loop over all bodies, calculate
+			// net forces and store in xforces and yforces
+			for(int k = 0; k < bodies.length; k++) { // loops through bodies 
+				xforces[k] = bodies[k].calcNetForceExertedByX(bodies); // stores net force x in xforces 
+				yforces[k] = bodies[k].calcNetForceExertedByY(bodies);
+			}                                                           // stores net force y in yforces
+			// TODO: loop over all bodies and call update
+			// with dt and corresponding xforces, yforces values
+			for(int k = 0; k < bodies.length; k+=1) {
+						bodies[k].update(dt, xforces[k], yforces[k]);
+			}
+			StdDraw.picture(0,0,"images/starfield.jpg");
+			
+			// TODO: loop over all bodies and call draw on each one
+			for(int k=0; k < bodies.length; k++)
+				bodies[k].draw();
+			
+			StdDraw.show(10);
 		}
-		double time = 0.0;
-		while (time <= T) {
-			double[] xForces = new double[bodys.length];
-			double[] yForces = new double[bodys.length];
-			for (int i = 0; i < bodys.length; i++) {
-				xForces[i] = bodys[i].calcNetForceExertedByX(bodys);
-				yForces[i] = bodys[i].calcNetForceExertedByY(bodys);
-			}
-			for (int i = 0; i < bodys.length; i++) {
-				bodys[i].update(dt, xForces[i], yForces[i]);
-			}
-
-			StdDraw.setScale(-radius, radius); 
-			StdDraw.picture(0, 0, "images/starfield.jpg"); 
-			for (int i = 0; i < bodys.length; i++) {
-				bodys[i].draw(); 
-			}
-			StdDraw.show(10); 
-			time += dt;
-		}
-
-		System.out.printf("%d\n", bodys.length);  
+		
+		// prints final values after simulation
+		
+		System.out.printf("%d\n", bodies.length);
 		System.out.printf("%.2e\n", radius);
-		for (int i = 0; i < bodys.length; i++) {
-			System.out.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n", bodys[i].myXPos, bodys[i].myYPos,
-					bodys[i].myXVel, bodys[i].myYVel, bodys[i].myMass, bodys[i].myFileName);
+		for (int i = 0; i < bodies.length; i++) {
+		    System.out.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+		   		              bodies[i].getX(), bodies[i].getY(), 
+		                      bodies[i].getXVel(), bodies[i].getYVel(), 
+		                      bodies[i].getMass(), bodies[i].getName());	
 		}
 	}
-
-	public static double readRadius(String fname) { 
-		File file = new File(fname);
-		Scanner s = null;
-		try {
-			s = new Scanner(file);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		s.nextDouble();
-		return s.nextDouble();
-
-	}
-
-	public static Body[] readBodies(String fname) { 
-		File file = new File(fname);
-		Scanner s = null;
-		try {
-			s = new Scanner(file);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		int arrayNum = s.nextInt();
-		Body[] bodyArray = new Body[arrayNum];
-		s.nextDouble();
-		for (int i = 0; i < arrayNum; i++) {
-			bodyArray[i] = new Body(s.nextDouble(), s.nextDouble(), s.nextDouble(), s.nextDouble(), s.nextDouble(),
-					s.next());
-		}
-
-		return bodyArray;
-	}
-
 }
